915a0b2e92664843a077d06f27d8d0169e37b35d
schau298 <schau298@mail.nmc.edu>
2021-09-20 13:21:02 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FutureValue.Models;
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        ViewBag.FV = 0;
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Index(FutureValueModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+        }
+        else
+        {
+            ViewBag.FV = 0;
+        }
+        
+        return View(model);
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    public IActionResult About()
+    {
+        return View();
+    }
+}
